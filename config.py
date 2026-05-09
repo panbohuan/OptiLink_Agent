9e7ea9e95220ad9741bb96ebae749e39947b83da
攀泊桓 <panbohuan@qq.com>
2026-05-09 22:38:34 +0800

    Deepseek api

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEEPSEEK_API_KEY = os.getenv("sk-04f27bbe66c74f709724ca7fa5f45d9f")
+DEEPSEEK_BASE_URL = "https://api.deepseek.com"
+DEEPSEEK_MODEL = "deepseek-v4-pro"
 
